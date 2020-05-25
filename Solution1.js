cba5ec3843e0cf133610006a508f869209c9e371
Officialflores4102 <officialflores4102@gmail.com>
2020-05-25 12:59:41 -0700

    last push

--- Solution1.js
+++ Solution1.js
@@ -10,38 +10,41 @@
             2 ON EACH SIDE
 */
 
-
 const cinemaBookingSearch = (n, reservedSeats) => {
-    
-    let maxFourPersonFamilies = null;
+	let maxFourPersonFamilies = null;
 
-    if (n != null && reservedSeats.length > 0) {
-        //if rows are provided, and if there are reserved seats, then go here
-        const seatRowMap = reservedSeats.reduce((map, [row, col]) => {
-            if (map.has(row)) {
-                map.get(row).push(col);
-            } else {
-                map.set(row, [col]);
-            }
-            return map;
-        }, new Map());
+	if (n != null && reservedSeats.length > 0) {
+		let maxInRow = null;
+		//if rows are provided, and if there are reserved seats, then go here
+		//Create an array with n arrays inside,
+		// work with bits at the reserved seat add a 1 for taken inside the row ,
+		//add 0 as the index for un reserved seats
+		// conditional if 0 index is repeated 4 times after the other then add it to maxInRow plus the prev result
+		//set maxFourpersonFamilies to maxinRow
+	} else if (n != null && reservedSeats.length === 0) {
+		//If rows are provided, and reservedSeats array is empty
+		//then get the number of available seats by mulitplying row and the 10 seats
+		//Math Floor works good here because the length of reservedSeats wont be a negative numeber
 
-        const 
+		let availableSeats = n * 10;
+		//Setting available seats to the sum of row, and seats available per row.
+		maxFourPersonFamilies = Math.floor(availableSeats / 4);
+	} else {
+		return;
+	}
 
-        
-    } else if (n != null && reservedSeats.length === 0) {
+	return maxFourPersonFamilies;
+};
 
-        //If rows are provided, and reservedSeats array is empty 
-        //then get the number of available seats by mulitplying row and the 10 seats
-        //Math Floor works good here because the length of reservedSeats wont be a negative numeber
-
-        let availableSeats = n * 10;
-        //Setting available seats to the sum of row, and seats available per row.
-        maxFourPersonFamilies = Math.floor(availableSeats/4)
-
-    } else {
-        return;
-    }
-    
-    return maxFourPersonFamilies;
-}
+const reservedSeats1 = [
+	[1, 2],
+	[1, 3],
+	[1, 8],
+	[2, 6],
+	[3, 1],
+	[3, 10],
+];
+const reservedSeats2 = [];
+console.log(cinemaBookingSearch(2, reservedSeats1));
+console.log(cinemaBookingSearch(3, reservedSeats2));
+console.log(cinemaBookingSearch(0, []));
